b11cce3836e3740192113aadf7d462c645773911
Atif-ak <81744577+Atif-ak@users.noreply.github.com>
2021-07-06 20:32:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Lift from './components/Lift'
+
+function App() {
+  return (
+    <div>
+     <Lift />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Lift.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./Lift.css";
+
+const Lift = () => {
+  const [slider, setSlider] = useState(949);
+
+  const styling = {
+    transform: `translateY(${slider}%)`,
+    transitionDuration: "3s",
+  };
+  return (
+    <div className="main">
+      <div className="lifts">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+
+      <div className="slider">
+        <div className="box2" style={styling}>
+          Lift
+        </div>
+      </div>
+      <div className="lifts">
+        <div className="box">9</div>
+        <div className="box">8</div>
+        <div className="box">7</div>
+        <div className="box">6</div>
+        <div className="box">5</div>
+        <div className="box">4</div>
+        <div className="box">3</div>
+        <div className="box">2</div>
+        <div className="box">1</div>
+        <div className="box">G</div>
+      </div>
+
+      <div className="numPad">
+        <div className="firstRow">
+          <div className="box3" onClick={() => setSlider(1)}>
+            9
+          </div>
+          <div className="box3" onClick={() => setSlider(106)}>
+            8
+          </div>
+          <div className="box3" onClick={() => setSlider(211)}>
+            7
+          </div>
+        </div>
+        <div className="secRow">
+          <div className="box3" onClick={() => setSlider(318)}>
+            6
+          </div>
+          <div className="box3" onClick={() => setSlider(423)}>
+            5
+          </div>
+          <div className="box3" onClick={() => setSlider(526)}>
+            4
+          </div>
+        </div>
+        <div className="thirdRow">
+          <div className="box3" onClick={() => setSlider(633)}>
+            3
+          </div>
+          <div className="box3" onClick={() => setSlider(738)}>
+            2
+          </div>
+          <div className="box3" onClick={() => setSlider(848)}>
+            1
+          </div>
+        </div>
+        <div className="lastRow">
+          <div className="box3" onClick={() => setSlider(949)}>
+            G
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Lift;
